f8e588bb871d902a0684b54eee17686b3d8991ee
VirendraDeshpande <Virendra_Deshpande@mastercard.com>
2015-05-15 12:23:45 +0530

    Sample

---
+++ WebApplication9/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TwitterBootstrapMVC;
+
+namespace WebApplication9
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Bootstrap.Configure();
+        }
+    }
+}
